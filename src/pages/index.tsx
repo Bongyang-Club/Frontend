a5371ee6851d0095c7aec00c81baf1804bab47ef
limjh5699 <limjh5699@gmail.com>
2023-03-10 11:10:44 +0900

    테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 const MainPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">테스트입니다</h1>;
 };
 
 export default MainPage;

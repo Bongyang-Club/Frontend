56ececac97b290e52d52cf7779c33ff034fe79df
jademon <j2python@gmail.com>
2023-04-10 16:22:26 +0900

    design width-4xl

--- src/components/club/subscribe/index.tsx
+++ src/components/club/subscribe/index.tsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import Content from "@/components/club/subscribe/content";
 
 const Club = () => {
-  const [user, setUser] = useState({ role: "" });
+  const [user, setUser] = useState({ role: "admin" });
   return (
     <div className="flex flex-col bg-[#Ffffff] max-w-4xl w-full px-10 pt-7 pb-10 drop-shadow-xl mt-10">
       <div className="flex flex-row justify-between items-center">
-        <div className="mx-2 mb-3 font-medium text-lg">내동아리</div>
+        <div className="mx-6 mb-3 font-medium text-lg">내동아리</div>
         {user.role === "admin" ? (
-          <button className="mx-2 mb-3 bg-[#D97706] text-white text-sm rounded-sm py-1 px-5">
+          <button className="mx-6 mb-3 bg-[#D97706] text-white text-sm rounded-sm py-1 px-5">
             홍보하기
           </button>
         ) : null}

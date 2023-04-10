6f338a17da95fc4b53a95fa81dc9d38a160fa57b
jademon <j2python@gmail.com>
2023-04-10 16:14:51 +0900

    add promotion contents

---
+++ src/components/club/promotion/content/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <>
+      <div className="w-[200px] h-[200px]">
+        <img src="http://placehold.it/200x200" />
+      </div>
+    </>
+  );
+};
+
+export default Content;
---
+++ src/components/club/promotion/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Content from "@/components/club/promotion/content";
+
+const Club = () => {
+  return (
+    <div className="flex flex-col bg-[#Ffffff] max-w-4xl w-full px-10 pt-7 pb-10 drop-shadow-xl mt-10">
+      <div className="mx-2 mb-3 font-medium text-lg">동아리원 모집중</div>
+      <div className="grid grid-cols-3 gap-x-5 gap-y-10 justify-items-center mt-3">
+        <Content />
+        <Content />
+        <Content />
+        <Content />
+        <Content />
+      </div>
+    </div>
+  );
+};
+
+export default Club;

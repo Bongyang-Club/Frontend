43feacc5fd335b124be3fd999355248c088865d4
jademon <j2python@gmail.com>
2023-04-11 16:06:29 +0900

    refactor

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleRight, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 import Link from "next/link";
+import User from "@/components/header/user";
 
 const Header = () => {
-  const [check, setCheck] = useState(false);
-  const [user, setUser] = useState({ studentId: 3208, name: "박대형" });
-
   return (
     <div className=" border-0 border-b border-[#DFDFDF] flex flex-row justify-center w-full">
       <div className="flex flex-row justify-between h-[64px] items-center max-w-6xl w-full px-5">
@@ -21,51 +17,7 @@ const Header = () => {
             />
           </Link>
         </div>
-        {!check ? (
-          <div
-            className="flex flex-row items-center cursor-pointer"
-            onClick={() => setCheck(true)}
-          >
-            <div className="text-[#D97706] font-medium pr-2 select-none">
-              {user.studentId} {user.name} 님
-            </div>
-            <FontAwesomeIcon
-              icon={faAngleRight}
-              className="text-[#D97706] pt-1 w-10"
-            />
-          </div>
-        ) : (
-          <div
-            className="flex flex-row items-center cursor-pointer"
-            onClick={() => setCheck(false)}
-          >
-            <div className="text-[#D97706] font-medium pr-2 select-none">
-              {user.studentId} {user.name} 님
-            </div>
-            <FontAwesomeIcon
-              icon={faAngleDown}
-              className="text-[#D97706] pt-1 w-10"
-            />
-            <div className="absolute top-16 z-10 mt-5 flex w-[10rem] -translate-x-5">
-              <div className="flex-auto w-[24px] rounded-lg bg-white text-sm shadow-lg ring-1 ring-gray-900/5">
-                <div className="group relative text-center py-3 px-4 hover:bg-gray-50 rounded-t-lg  border-b">
-                  <div>
-                    <a href="#" className="font-regular text-gray-900">
-                      마이페이지
-                    </a>
-                  </div>
-                </div>
-                <div className="group relative text-center py-3 px-4 hover:bg-gray-50  rounded-b-lg">
-                  <div>
-                    <a href="#" className="font-regular text-gray-900">
-                      로그아웃
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+        <User />
       </div>
     </div>
   );
---
+++ src/components/header/user/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
+const User = () => {
+  const [check, setCheck] = useState(false);
+  const [user, setUser] = useState({ studentId: 3208, name: "박대형" });
+  return !check ? (
+    <div
+      className="flex flex-row items-center cursor-pointer"
+      onClick={() => setCheck(true)}
+    >
+      <div className="text-[#D97706] font-medium pr-2 select-none">
+        {user.studentId} {user.name} 님
+      </div>
+      <FontAwesomeIcon
+        icon={faAngleRight}
+        className="text-[#D97706] pt-1 w-10"
+      />
+    </div>
+  ) : (
+    <div
+      className="flex flex-row items-center cursor-pointer"
+      onClick={() => setCheck(false)}
+    >
+      <div className="text-[#D97706] font-medium pr-2 select-none">
+        {user.studentId} {user.name} 님
+      </div>
+      <FontAwesomeIcon
+        icon={faAngleDown}
+        className="text-[#D97706] pt-1 w-10"
+      />
+      <div className="absolute top-16 z-10 mt-5 flex w-[10rem] -translate-x-5">
+        <div className="flex-auto w-[24px] rounded-lg bg-white text-sm shadow-lg ring-1 ring-gray-900/5">
+          <div className="group relative text-center py-3 px-4 hover:bg-gray-50 rounded-t-lg  border-b">
+            <div>
+              <a href="#" className="font-regular text-gray-900">
+                마이페이지
+              </a>
+            </div>
+          </div>
+          <div className="group relative text-center py-3 px-4 hover:bg-gray-50  rounded-b-lg">
+            <div>
+              <a href="#" className="font-regular text-gray-900">
+                로그아웃
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;

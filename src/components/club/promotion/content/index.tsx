401471298db082482ca633c78c57fa4d02a3e2ea
jademon <j2python@gmail.com>
2023-04-11 09:16:53 +0900

    add cursor pointer

--- src/components/club/promotion/content/index.tsx
+++ src/components/club/promotion/content/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Content = () => {
   return (
     <>
-      <div className="w-[200px] h-[200px]">
+      <div className="w-[200px] h-[200px] cursor-pointer">
         <img src="http://placehold.it/200x200" />
       </div>
     </>
--- src/components/club/subscribe/content/index.tsx
+++ src/components/club/subscribe/content/index.tsx
@@ -5,11 +5,11 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 const Content = () => {
   const [data, setData] = useState();
   return data ? (
-    <div className="w-[200px] h-[200px]">
+    <div className="w-[200px] h-[200px] cursor-pointer">
       <img src="http://placehold.it/200x200" />
     </div>
   ) : (
-    <div className="w-[200px] h-[200px] flex justify-center items-center bg-[#CCCCCC]">
+    <div className="w-[200px] h-[200px] flex justify-center items-center bg-[#CCCCCC] cursor-pointer">
       <FontAwesomeIcon icon={faPlus} />
     </div>
   );

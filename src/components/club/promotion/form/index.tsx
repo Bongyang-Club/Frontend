9e357d8dde6e07f307571a5fb1e53f749c469086
jademon <j2python@gmail.com>
2023-04-19 00:03:20 +0900

    any

--- src/components/club/promotion/form/index.tsx
+++ src/components/club/promotion/form/index.tsx
@@ -1,85 +1,89 @@
 const Form = () => {
   return (
-    <div className="w-full h-[100.3rem]">
-      <div className="max-w-4xl h-[100rem] mt-10 mx-auto shadow-[0_0_8px_0_rgba(0,0,0,0.3)] flex justify-center bg-white ">
-        <div className="mt-[5rem] w-[38rem] relative">
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="max-w-4xl w-full h-full my-10 py-20 shadow-xl flex justify-center bg-white ">
+        <div className="w-[38rem]">
           {/* 탑 */}
           <div className="w-full flex justify-between">
-            <span className="font-bold font text-3xl">동아리 홍보 신청</span>
-            <button className="w-[7rem] bg-[#D97706] text-white">신청</button>
+            <span className="font-bold font text-2xl">동아리 홍보 신청</span>
+            <button className="w-[6rem] bg-[#D97706] text-white">신청</button>
           </div>
           {/* 동아리 명 */}
-          <div className="w-full flex justify-between mt-[3rem]">
-            <span className="text-xl text-[#676767] leading-10">동아리명</span>
+          <div className="w-full flex items-center justify-between mt-[3rem]">
+            <span className="text-lg text-[#676767] leading-10">동아리명</span>
             <input
-              className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] bg-[#F8F8F8] px-3 text-[1.3rem]"
+              className="w-[28.5rem] border border-[#DDDDDD] bg-[#F8F8F8] px-2 py-1 text-lg"
               value={"봉양클럽"}
               readOnly={true}
             />
           </div>
           {/* 지원방법 */}
-          <div className="w-full flex justify-between mt-[1.5rem] relative">
-            <span className="text-xl text-[#676767] leading-10">지원방법</span>
-            <div className="w-[28.5rem] flex">
-              <div className="w-[6rem] flex align-middle">
-                <input type="checkBox" className="w-[1.4rem] h-[2.5rem]" />
-                <span className="ml-[0.7rem] leading-10 text-[1.2rem] text-[#B1B1B1] ">
+          <div className="w-full flex items-center justify-between mt-[1.5rem] relative">
+            <span className="text-lg text-[#676767] leading-10">지원방법</span>
+            <div className="w-[28.5rem] flex items-center">
+              <div className="w-[6rem] flex align-middle items-center">
+                <input type="checkBox" className="w-[1.2rem] h-[1.2rem]" />
+                <span className="ml-[0.7rem] leading-10 text-md text-[#B1B1B1] ">
                   구글폼
                 </span>
               </div>
-              <div className="w-[6rem] ml-[2rem] flex align-middle">
-                <input type="checkBox" className="w-[1.4rem] h-[2.5rem]" />
-                <span className="ml-[0.7rem] leading-10 text-[1.2rem] text-[#B1B1B1] ">
+              <div className="w-[6rem] ml-[2rem] flex align-middle items-center">
+                <input type="checkBox" className="w-[1.2rem] h-[1.2rem]" />
+                <span className="ml-[0.7rem] leading-10 text-md text-[#B1B1B1] ">
                   면접
                 </span>
               </div>
-              <div className="w-[6rem] ml-[2rem] flex align-middle">
-                <input type="checkBox" className="w-[1.4rem] h-[2.5rem]" />
-                <span className="ml-[0.7rem] leading-10 text-[1.2rem] text-[#B1B1B1] ">
+              <div className="w-[6rem] ml-[2rem] flex align-middle items-center">
+                <input type="checkBox" className="w-[1.2rem] h-[1.2rem]" />
+                <span className="ml-[0.7rem] leading-10 text-md text-[#B1B1B1] ">
                   테스트
                 </span>
               </div>
             </div>
-            <span className="absolute right-0 bottom-0 translate-y-6 text-[0.8rem] text-[#B1B1B1]">
+            <span className="absolute right-0 bottom-0 translate-y-6 text-[#B1B1B1] text-xs font-thin">
               ※ 선택하지 않으면 바로 동아리에 가입됩니다.
             </span>
           </div>
           {/* 정보 */}
-          <div className="w-full flex justify-end">
+          <div className="w-full flex justify-end items-center ">
             <div className="w-[35rem] mt-[2rem]">
               {/* 링크 */}
               <div className="w-full flex justify-between">
-                <span className="text-xl text-[#676767] leading-10">링크</span>
-                <input className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
+                <span className="text-lg text-[#676767] leading-10">링크</span>
+                <input className="w-[28.5rem] text-lg px-2 py-1 border border-[#DDDDDD]" />
               </div>
               {/* 신청기간 */}
               <div className="w-full flex justify-between mt-[2rem]">
-                <span className="text-xl text-[#676767] leading-10">
+                <span className="text-lg text-[#676767] leading-10">
                   신청기간
                 </span>
-                <input className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
+                <input className="w-[28.5rem] text-lg px-2 py-1 border border-[#DDDDDD]" />
               </div>
               {/* 면접 */}
               <div className="w-full flex justify-between mt-[2rem]">
-                <span className="text-xl text-[#676767] leading-10">면접</span>
+                <span className="text-lg text-[#676767] leading-10">면접</span>
                 <div className="w-[28.5rem] flex relative">
                   <div className="flex align-middle">
                     <input
                       type="radio"
                       name="interview"
-                      className="w-[1.4rem] h-[2.5rem] accent-gray-500"
+                      className="w-[1.4rem] accent-gray-500"
                     />
-                    <span className="ml-2 leading-9 text-[1.3rem]">개인별</span>
+                    <span className="ml-2 leading-9 text-md text-[#B1B1B1]">
+                      개인별
+                    </span>
                   </div>
                   <div className="ml-8 flex align-middle">
                     <input
                       type="radio"
                       name="interview"
-                      className="w-[1.4rem] h-[2.5rem] accent-gray-500"
+                      className="w-[1.4rem] accent-gray-500"
                     />
-                    <span className="ml-2 leading-9 text-[1.3rem]">전체</span>
+                    <span className="ml-2 leading-9 text-md text-[#B1B1B1]">
+                      전체
+                    </span>
                   </div>
-                  <span className="absolute bottom-0 translate-y-7 text-[#B1B1B1] text-[0.8rem]">
+                  <span className="absolute bottom-0 translate-y-7 text-[#B1B1B1] text-xs font-thin">
                     ※ 개인별의 경우 신청자 확인후 면접 일정을 공지해주시기
                     바랍니다.
                   </span>
@@ -87,38 +91,42 @@ const Form = () => {
               </div>
               {/* 테스트 */}
               <div className="w-full flex justify-between mt-[2rem]">
-                <span className="text-xl text-[#676767] leading-10">
+                <span className="text-lg text-[#676767] leading-10">
                   테스트
                 </span>
                 <div className="w-[28.5rem] flex">
                   <div className="flex align-middle">
                     <input
                       type="radio"
-                      name="test"
-                      className="w-[1.4rem] h-[2.5rem] accent-gray-500"
+                      name="interview"
+                      className="w-[1.4rem] accent-gray-500"
                     />
-                    <span className="ml-2 leading-9 text-[1.3rem]">개인별</span>
+                    <span className="ml-2 leading-9 text-md text-[#B1B1B1]">
+                      개인별
+                    </span>
                   </div>
                   <div className="ml-8 flex align-middle">
                     <input
                       type="radio"
-                      name="test"
-                      className="w-[1.4rem] h-[2.5rem] accent-gray-500"
+                      name="interview"
+                      className="w-[1.4rem] accent-gray-500"
                     />
-                    <span className="ml-2 leading-9 text-[1.3rem]">전체</span>
+                    <span className="ml-2 leading-9 text-md text-[#B1B1B1]">
+                      전체
+                    </span>
                   </div>
                 </div>
               </div>
               {/* 시간 */}
               <div className="w-full flex justify-between mt-[1.5rem]">
-                <span className="text-xl text-[#676767] leading-10">시간</span>
-                <input className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
+                <span className="text-lg text-[#676767] leading-10">시간</span>
+                <input className="w-[28.5rem] text-lg px-2 py-1 border border-[#DDDDDD]" />
               </div>
               {/* 장소 */}
               <div className="w-full flex justify-between mt-[1.5rem]">
-                <span className="text-xl text-[#676767] leading-10">장소</span>
+                <span className="text-lg text-[#676767] leading-10">장소</span>
                 <div className="relative flex">
-                  <input className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
+                  <input className="w-[28.5rem] text-lg px-2 py-1 border border-[#DDDDDD]" />
                   <span className="absolute bottom-0 translate-y-10 text-[#B1B1B1] text-[0.8rem]">
                     ※ 아직 정해지지 않았다면 신청자 확인후 공지해주시기
                     바랍니다.
@@ -129,60 +137,60 @@ const Form = () => {
           </div>
           {/* 모집대상 */}
           <div className="w-full flex justify-between mt-[4rem]">
-            <span className="text-xl text-[#676767] leading-10">모집대상</span>
+            <span className="text-lg text-[#676767] leading-10">모집대상</span>
             <div className="flex flex-col">
               <div className="w-[28.5rem]">
-                <input className="w-[21rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
-                <input className="w-[5rem] h-[2.5rem] ml-2 border border-[#DDDDDD] px-3" />
-                <span className="ml-2 text-[1.5rem]">명</span>
+                <input className="w-[21rem] border border-[#DDDDDD] text-lg px-2 py-1" />
+                <input className="w-[5rem] ml-2 border border-[#DDDDDD] text-lg px-2 py-1" />
+                <span className="ml-2 text-lg">명</span>
               </div>
               <div className="w-[28.5rem] mt-2">
-                <input className="w-[21rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
-                <input className="w-[5rem] h-[2.5rem] ml-2 border border-[#DDDDDD] px-3" />
-                <span className="ml-2 text-[1.5rem]">명</span>
+                <input className="w-[21rem] border border-[#DDDDDD] text-lg px-2 py-1" />
+                <input className="w-[5rem] ml-2 border border-[#DDDDDD] text-lg px-2 py-1" />
+                <span className="ml-2 text-lg">명</span>
               </div>
               <div className="w-[28.5rem] mt-2">
-                <input className="w-[21rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
-                <input className="w-[5rem] h-[2.5rem] ml-2 border border-[#DDDDDD] px-3" />
-                <span className="ml-2 text-[1.5rem]">명</span>
+                <input className="w-[21rem] border border-[#DDDDDD] text-lg px-2 py-1" />
+                <input className="w-[5rem] ml-2 border border-[#DDDDDD] text-lg px-2 py-1" />
+                <span className="ml-2 text-lg">명</span>
               </div>
               <div className="w-[28.5rem] mt-2">
-                <input className="w-[21rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
-                <input className="w-[5rem] h-[2.5rem] ml-2 border border-[#DDDDDD] px-3" />
-                <span className="ml-2 text-[1.5rem]">명</span>
+                <input className="w-[21rem] border border-[#DDDDDD] text-lg px-2 py-1" />
+                <input className="w-[5rem] ml-2 border border-[#DDDDDD] text-lg px-2 py-1" />
+                <span className="ml-2 text-lg">명</span>
               </div>
               <div className="w-[28.5rem] mt-2">
-                <input className="w-[21rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
-                <input className="w-[5rem] h-[2.5rem] ml-2 border border-[#DDDDDD] px-3" />
-                <span className="ml-2 text-[1.5rem]">명</span>
+                <input className="w-[21rem] border border-[#DDDDDD] text-lg px-2 py-1" />
+                <input className="w-[5rem] ml-2 border border-[#DDDDDD] text-lg px-2 py-1" />
+                <span className="ml-2 text-lg">명</span>
               </div>
               <div className="w-[28.5rem] mt-2">
-                <input className="w-[21rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
-                <input className="w-[5rem] h-[2.5rem] ml-2 border border-[#DDDDDD] px-3" />
-                <span className="ml-2 text-[1.5rem]">명</span>
+                <input className="w-[21rem] border border-[#DDDDDD] text-lg px-2 py-1" />
+                <input className="w-[5rem] ml-2 border border-[#DDDDDD] text-lg px-2 py-1" />
+                <span className="ml-2 text-lg">명</span>
               </div>
             </div>
           </div>
           {/* 활동 방법 */}
           <div className="w-full flex justify-between mt-[2rem]">
-            <span className="text-xl text-[#676767]">활동 방법</span>
-            <textarea className="w-[28.5rem] h-[15rem] border border-[#DDDDDD] px-3 resize-none" />
+            <span className="text-lg text-[#676767]">활동 방법</span>
+            <textarea className="w-[28.5rem] h-[15rem] border border-[#DDDDDD] p-2 resize-none" />
           </div>
           {/* 활동 장소 */}
           <div className="w-full flex justify-between mt-[2rem]">
-            <span className="text-xl text-[#676767] leading-10">활동 장소</span>
-            <input className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
+            <span className="text-lg text-[#676767] leading-10">활동 장소</span>
+            <input className="w-[28.5rem] border border-[#DDDDDD] text-lg px-2 py-1" />
           </div>
           {/* 활동 시간 */}
           <div className="w-full flex justify-between mt-[2rem]">
-            <span className="text-xl text-[#676767] leading-10">활동 시간</span>
-            <input className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3" />
+            <span className="text-lg text-[#676767] leading-10">활동 시간</span>
+            <input className="w-[28.5rem] border border-[#DDDDDD] text-lg px-2 py-1" />
           </div>
           {/* 가입문의 */}
           <div className="w-full flex justify-between mt-[2rem]">
-            <span className="text-xl text-[#676767] leading-10">가입문의</span>
+            <span className="text-lg text-[#676767] leading-10">가입문의</span>
             <input
-              className="w-[28.5rem] h-[2.5rem] border border-[#DDDDDD] px-3"
+              className="w-[28.5rem] border border-[#DDDDDD] text-lg px-2 py-1 font-normal"
               placeholder="ex. 인스타 : wlqdp_rkffo"
             />
           </div>

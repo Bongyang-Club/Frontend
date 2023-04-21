419af7bb6c76dea23673f08dd1c8bcca907cbc37
jademon <j2python@gmail.com>
2023-04-22 04:00:53 +0900

    refactor link

--- src/components/club/home/introduce/menu/admin/index.tsx
+++ src/components/club/home/introduce/menu/admin/index.tsx
@@ -8,7 +8,7 @@ const Menu = () => {
     <div className="max-w-[26rem] w-full flex flex-col justify-center items-center">
       <div className="select-none font-semibold text-4xl mb-8">{club.name}</div>
       <Link
-        href="/member"
+        href="/"
         className="cursor-pointer border text-center py-1 my-2 rounded-sm w-36 border-[#B1B1B1] text-[#B1B1B1] hover:text-white hover:bg-[#B1B1B1]"
       >
         동아리장 변경
@@ -20,7 +20,7 @@ const Menu = () => {
         공지 등록
       </Link>
       <Link
-        href="/"
+        href="/club/promotion"
         className="cursor-pointer border text-center py-1 my-2 rounded-sm w-36 border-[#B1B1B1] text-[#B1B1B1] hover:text-white hover:bg-[#B1B1B1]"
       >
         동아리 홍보하기
@@ -32,7 +32,7 @@ const Menu = () => {
         가입신청 확인
       </Link>
       <Link
-        href="/"
+        href="/member"
         className="cursor-pointer border text-center py-1 my-2 rounded-sm w-36 border-[#B1B1B1] text-[#B1B1B1] hover:text-white hover:bg-[#B1B1B1]"
       >
         동아리원 확인

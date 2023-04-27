8bc268e031ab0b218d37aed0aa696c94e9aaa3a5
limjh5699 <limjh5699@gmail.com>
2023-04-27 17:58:45 +0900

    [FIX] hide header for login path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
@@ -8,12 +9,14 @@ import Header from "@/components/header";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <div className="bg-[#F5F5F5] min-h-screen">
       <Head>
         <title>Bongyang-Club</title>
       </Head>
-      <Header />
+      {router.pathname == "/login" ? <></> : <Header />}
       <Component {...pageProps} />
     </div>
   );
